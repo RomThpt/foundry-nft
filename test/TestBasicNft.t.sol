3e066ec1d08a36a6d84a2eca770e1e7c15ee1261
ReeFLk <reeflk8@gmail.com>
2024-11-13 13:01:51 +0100

    modules

--- test/TestBasicNft.t.sol
+++ test/TestBasicNft.t.sol
@@ -8,6 +8,7 @@ import {BasicNft} from "../src/BasicNft.sol";
 contract TestBasicNft is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
 
     function setUp() public {
         deployer = new DeployBasicNft();
@@ -15,8 +16,22 @@ contract TestBasicNft is Test {
     }
 
     function testNameIsCorrect() public view {
-        string memory expectedName = "Dogie";
+        string memory expectedName = "Doggie";
         string memory actualName = basicNft.name();
-        assertEq(expectedName, actualName);
+        assertEq(keccak256(abi.encodePacked(expectedName)), keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        vm.deal(USER, 1 ether);
+        basicNft.mintNft("ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json");
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(
+                abi.encodePacked(
+                    "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json"
+                )
+            ) == keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
     }
 }
